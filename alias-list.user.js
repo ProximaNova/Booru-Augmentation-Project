efdf40877f556f02fba0ada0d07519dd2b7cf6a4
ProximaNova <jaretrudd@gmail.com>
2016-03-30 02:35:40 -0600

    colored tags

--- alias-list.user.js
+++ alias-list.user.js
@@ -9,16 +9,36 @@
 document.getElementsByTagName("div")[2].style.color = "black";
 document.getElementsByTagName("div")[2].innerHTML = "<b>One example:</b> <code style='color:#A0A'>Evangelion</code> is the tag and <code style='color:#A0A'>Neon_Genesis_Evangelion</code> is the alias.";
 document.getElementsByTagName("th")[2].style.display = "none";
-for (i = 2; i < document.getElementsByTagName("td").length - 4; i += 3) {
-    document.getElementsByTagName("td")[i].style.display = "none";
+var getTd = document.getElementsByTagName("td");
+
+for (i = 2; i < getTd.length - 4; i += 3) {
+    getTd[i].style.display = "none";
 }
 for (i = 0; i < document.getElementsByTagName("td").length - 4; i += 3) {
-    document.getElementsByTagName("td")[i].innerHTML = "<a href='index.php?page=post&s=list&tags=" +
-    document.getElementsByTagName("td")[i].innerHTML + "'>" +
-    document.getElementsByTagName("td")[i].innerHTML + "</a>"
+    if (getTd[i].innerHTML.match(/_\(artist\)/g)) {
+        var coloredTags = " style='color:#A00;'";
+    } else if (getTd[i].innerHTML.match(/_\(character\)/g)) {
+        var coloredTags = " style='color:#0A0;'";
+    } else if (getTd[i].innerHTML.match(/_\(copyright\)/g)) {
+        var coloredTags = " style='color:#A0A;'";
+    } else {
+        var coloredTags = "";
+    }
+    getTd[i].innerHTML = "<a href='index.php?page=post&s=list&tags=" +
+    getTd[i].innerHTML + "'" + coloredTags + ">" +
+    getTd[i].innerHTML + "</a>"
 }
 for (i = 1; i < document.getElementsByTagName("td").length - 4; i += 3) {
-    document.getElementsByTagName("td")[i].innerHTML = "<a href='index.php?page=post&s=list&tags=" +
-    document.getElementsByTagName("td")[i].innerHTML + "'>" +
-    document.getElementsByTagName("td")[i].innerHTML + "</a>"
+    if (getTd[i].innerHTML.match(/_\(artist\)/g)) {
+        var coloredTags = " style='color:#A00;'";
+    } else if (getTd[i].innerHTML.match(/_\(character\)/g)) {
+        var coloredTags = " style='color:#0A0;'";
+    } else if (getTd[i].innerHTML.match(/_\(copyright\)/g)) {
+        var coloredTags = " style='color:#A0A;'";
+    } else {
+        var coloredTags = "";
+    }
+    getTd[i].innerHTML = "<a href='index.php?page=post&s=list&tags=" +
+    getTd[i].innerHTML + "'" + coloredTags + ">" +
+    getTd[i].innerHTML + "</a>"
 }
