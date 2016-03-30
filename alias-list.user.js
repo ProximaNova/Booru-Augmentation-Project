e1305a513ef71a68fb3a59ce6d3a42c850e5dd9f
ProximaNova <jaretrudd@gmail.com>
2016-03-30 02:48:50 -0600

    hover color and other stuff

--- alias-list.user.js
+++ alias-list.user.js
@@ -6,21 +6,31 @@
 // @grant       none
 // ==/UserScript==
 
-document.getElementsByTagName("div")[2].style.color = "black";
-document.getElementsByTagName("div")[2].innerHTML = "<b>One example:</b> <code style='color:#A0A'>Evangelion</code> is the tag and <code style='color:#A0A'>Neon_Genesis_Evangelion</code> is the alias.";
-document.getElementsByTagName("th")[2].style.display = "none";
-var getTd = document.getElementsByTagName("td");
+document.getElementsByTagName("div")[2].style.color =
+"black";
+document.getElementsByTagName("div")[2].innerHTML =
+"<b>One example:</b> \
+<a href='index.php?page=post&s=list&tags=evangelion' style='color:#A0A' onmouseover=\"this.style.color = \
+'#9093ff'\" onmouseout=\"this.style.color = '#A0A'\">Evangelion</a> is the tag and \
+<a href='index.php?page=post&s=list&tags=neon_genesis_evangelion' style='color:#A0A' onmouseover=\"this.style.color = \
+'#9093ff'\" onmouseout=\"this.style.color = '#A0A'\">Neon_Genesis_Evangelion</a> is the alias.";
+document.getElementsByTagName("th")[2].style.display =
+"none";
 
+var getTd = document.getElementsByTagName("td");
 for (i = 2; i < getTd.length - 4; i += 3) {
     getTd[i].style.display = "none";
 }
 for (i = 0; i < document.getElementsByTagName("td").length - 4; i += 3) {
     if (getTd[i].innerHTML.match(/_\(artist\)/g)) {
-        var coloredTags = " style='color:#A00;'";
+        var coloredTags = " style='color:#A00;' onmouseover=\"this.style.color = \
+                           '#9093ff'\" onmouseout=\"this.style.color = '#A00'\"";
     } else if (getTd[i].innerHTML.match(/_\(character\)/g)) {
-        var coloredTags = " style='color:#0A0;'";
+        var coloredTags = " style='color:#0A0;' onmouseover=\"this.style.color = \
+                           '#9093ff'\" onmouseout=\"this.style.color = '#0A0'\"";
     } else if (getTd[i].innerHTML.match(/_\(copyright\)/g)) {
-        var coloredTags = " style='color:#A0A;'";
+        var coloredTags = " style='color:#A0A;' onmouseover=\"this.style.color = \
+                           '#9093ff'\" onmouseout=\"this.style.color = '#A0A'\"";
     } else {
         var coloredTags = "";
     }
@@ -30,11 +40,14 @@ for (i = 0; i < document.getElementsByTagName("td").length - 4; i += 3) {
 }
 for (i = 1; i < document.getElementsByTagName("td").length - 4; i += 3) {
     if (getTd[i].innerHTML.match(/_\(artist\)/g)) {
-        var coloredTags = " style='color:#A00;'";
+        var coloredTags = " style='color:#A00;' onmouseover=\"this.style.color = \
+                           '#9093ff'\" onmouseout=\"this.style.color = '#A00'\"";
     } else if (getTd[i].innerHTML.match(/_\(character\)/g)) {
-        var coloredTags = " style='color:#0A0;'";
+        var coloredTags = " style='color:#0A0;' onmouseover=\"this.style.color = \
+                           '#9093ff'\" onmouseout=\"this.style.color = '#0A0'\"";
     } else if (getTd[i].innerHTML.match(/_\(copyright\)/g)) {
-        var coloredTags = " style='color:#A0A;'";
+        var coloredTags = " style='color:#A0A;' onmouseover=\"this.style.color = \
+                           '#9093ff'\" onmouseout=\"this.style.color = '#A0A'\"";
     } else {
         var coloredTags = "";
     }
